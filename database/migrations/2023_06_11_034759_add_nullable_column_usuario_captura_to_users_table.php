f257d333a02512005e95b0acf15abdb951a27f89
zlAxel <axel.macias17@hotmail.com>
2023-06-10 21:58:12 -0600

    Se agregó el nullable a la columna usuario_captura

--- database/migrations/2023_06_11_034759_add_nullable_column_usuario_captura_to_users_table.php
+++ database/migrations/2023_06_11_034759_add_nullable_column_usuario_captura_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('usuario_captura')->nullable()->change();
+            $table->string('usuario_captura')->nullable()->default('MANUAL')->change();
         });
     }
 
