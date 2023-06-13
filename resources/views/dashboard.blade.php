ec7db937e13e52f5880e489892b820115ca818b1
DanielTellezH <danielth681@gmail.com>
2023-06-12 20:12:22 -0600

    Se cambió el mensaje de eliminar integrante

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,27 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
+@section('titulo')
+    Inicio
+@endsection
+
+@section('contenido')
+    <div class="text-2xl font-bold text-center mt-10 mb-10">
+        Hola jsjs
     </div>
-</x-app-layout>
+
+    {{-- <livewire:show-proyectos /> --}}
+
+@endsection
+
+{{-- ========================================= --}}
+{{-- Estilos y scripts adicionales --}}
+
+@push('styles')
+    {{-- Estilos adicionales --}}
+@endpush
+
+@push('scripts')
+    {{-- Scripts adicionales --}}
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+@endpush
